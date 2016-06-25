287026758ea50bbe81c0882c23a4a05e94607552
Kimiko Kano <kskano@gmail.com>
2016-06-25 12:42:44 -0600

    Add answer description validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :poll
   has_many :votes, dependent: :destroy
+  validates_presence_of :description
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :poll }
   it { should have_many(:votes).dependent(:destroy) }
+  it { should validate_presence_of :description }
 end
